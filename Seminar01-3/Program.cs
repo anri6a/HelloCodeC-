f9e7d911303af2f9ebd89e051ac365b7f5279d55
Andrei Shyrakou <anri6a@gmail.com>
2023-02-13 14:06:12 +0300

    3 task about square done

--- Seminar01-3/Program.cs
+++ Seminar01-3/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*из 2х введенных чисел определяем является ли первое 
+квадратом второго*/
+
+        double x;
+        double y;
+        
+        Console.WriteLine("введи х");
+        x = inputValue();
+        Console.WriteLine("введи y");
+        y = inputValue();
+        if (x == y * y)
+        {
+            Console.WriteLine("kvadrat");
+        }
+        else
+        {
+            Console.WriteLine("ne kvadrat");
+        }
+    
+     static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result));
+        return result;
+    }
+
+
