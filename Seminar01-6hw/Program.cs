5b13ae5ab6f9b4d34b7c859e4d650fb8b3fc19f5
Andrei Shyrakou <anri6a@gmail.com>
2023-02-14 17:00:21 +0300

    seminar 01 task 6 home work is number even done

---
+++ Seminar01-6hw/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Напишите программу, которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).*/
+Console.WriteLine("enter number");
+int number = Convert.ToInt16(Console.ReadLine());
+if (number % 2 == 0)
+Console.WriteLine("number is even");
+else 
+Console.WriteLine("number is not even");
+
