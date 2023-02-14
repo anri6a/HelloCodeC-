003929ab7f4992a886c9db4e224d2e8d306b71b3
Andrei Shyrakou <anri6a@gmail.com>
2023-02-14 13:58:21 +0300

    seminar 01 task 2 home work bolshe/menshe

---
+++ Seminar01-2hw/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее.*/
+int a = new Random().Next(0, 100);
+int b = new Random().Next(0, 100);
+if (a > b)
+Console.WriteLine(a + " bolshe; " + b + " menshe");
+else 
+if (a < b)
+    Console.WriteLine(a + " menshe; " + b + " bolshe");
+else 
+    Console.WriteLine(a + " ravno " + b);
+
+
+
