a5fc2b9c896aaf2ea202b460f88cec34bdc82901
Andrei Shyrakou <anri6a@gmail.com>
2023-02-14 13:45:03 +0300

    seminar 01 task 4 -N..N  done

---
+++ Seminar01-4/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая на вход принимает одно число (N), 
+а на выходе показывает все целые числа в промежутке от -N до N.
+*/
+Console.WriteLine("Enter number");
+int number = Convert.ToInt16(Console.ReadLine());
+int count = -number;
+;
+
+while (count <= number)
+{
+    Console.Write(count + " ");
+    count++;
+}
