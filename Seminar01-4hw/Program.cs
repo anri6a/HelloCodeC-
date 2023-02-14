22343058063bff54aac46c0b3049ebf825ce43c3
Andrei Shyrakou <anri6a@gmail.com>
2023-02-14 17:55:30 +0300

    seminar 01 task 4 home work max from a b c with array does not work???

--- Seminar01-4hw/Program.cs
+++ Seminar01-4hw/Program.cs
@@ -1,8 +1,8 @@
 ﻿/*Напишите программу, которая принимает на вход три числа и 
 выдаёт максимальное из этих чисел.*/
-int a = new Random().Next(0, 100);
-int b = new Random().Next(0, 100);
-int c = new Random().Next(0, 100);
+int a = new Random().Next(1, 100);
+int b = new Random().Next(1, 100);
+int c = new Random().Next(1, 100);
 Console.WriteLine(a);
 Console.WriteLine(b);
 Console.WriteLine(c);
@@ -14,3 +14,13 @@ Console.WriteLine("maximal " + b);
 else 
 if (c > a && c > b)
 Console.WriteLine("maximal " + c);
+
+
+/*int [] array = new int(a, b, c);
+int max = 0;
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] > max)
+max = array[i];
+}
+Console.WriteLine("maximal " + max);*/
