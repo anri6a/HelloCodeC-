52ada9fcd47d82be4291025f95de91c6e5cba390
Andrei Shyrakou <anri6a@gmail.com>
2023-02-14 14:26:06 +0300

    seminar 01 task 4 home work max from a b c done

---
+++ Seminar01-4hw/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая принимает на вход три числа и 
+выдаёт максимальное из этих чисел.*/
+int a = new Random().Next(0, 100);
+int b = new Random().Next(0, 100);
+int c = new Random().Next(0, 100);
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+if (a > b && a > c)
+    Console.WriteLine("maximal " + a);
+else 
+if (b > a && b > c)
+Console.WriteLine("maximal " + b);
+else 
+if (c > a && c > b)
+Console.WriteLine("maximal " + c);
