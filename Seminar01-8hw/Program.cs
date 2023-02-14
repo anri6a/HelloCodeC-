f078b7607ad11c79b7d1a9fea2a91c523afe85d4
Andrei Shyrakou <anri6a@gmail.com>
2023-02-14 17:09:55 +0300

    seminar 01 task home work all even numbers befor N done

---
+++ Seminar01-8hw/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.*/
+int number = new Random().Next(1, 50);
+Console.WriteLine("number = " + number);
+for (int i = 1; i < number+1; i++)
+if (i % 2 == 0)
+    Console.Write(i+" ");
+
+
