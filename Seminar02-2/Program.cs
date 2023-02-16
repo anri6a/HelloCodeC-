df07292df78f308a60d33eca95f493688f9b8271
Andrei Shyrakou <anri6a@gmail.com>
2023-02-16 16:06:38 +0300

    seminar 02 2 del 2nd digit from Random(xxx) done

---
+++ Seminar02-2/Program.cs
@@ -0,0 +1,8 @@
+﻿/*Del 2d digit from Random(xxx)*/
+int number = new Random().Next(100, 1000);
+Console.WriteLine("number = " + number);
+int firstDigit = number / 100;
+int secondDigit = (number % 100) / 10;
+int thirdDigit = (number % 100) % 10;
+int result = firstDigit * 10 + thirdDigit;
+Console.WriteLine("result = " + result);
