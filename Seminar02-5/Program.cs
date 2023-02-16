3aee51792224a7ab33d054ca05207b7ac0bb1db8
Andrei Shyrakou <anri6a@gmail.com>
2023-02-16 16:50:30 +0300

    seminar 02 5 is number squer another done

---
+++ Seminar02-5/Program.cs
@@ -0,0 +1,6 @@
+﻿int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2 || number2 == number1 * number1) 
+Console.WriteLine("squer");
+else Console.WriteLine("not squer");
+
