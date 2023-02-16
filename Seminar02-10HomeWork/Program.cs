1768d62269c45809330231b50521addae924f71c
Andrei Shyrakou <anri6a@gmail.com>
2023-02-16 17:02:56 +0300

    seminar 02 10 home work 2nd digit of number done

---
+++ Seminar02-10HomeWork/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.*/
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+
+// int firstDigit = number / 100;
+// int secondDigit = (number / 10) % 10;
+// int thirdDigit = number % 10;
+
+Console.WriteLine("second digit = " + ((number / 10) % 10));
