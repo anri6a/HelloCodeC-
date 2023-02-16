6d815b7fd82aa572ba6b1bef620a2278402d119c
Andrei Shyrakou <anri6a@gmail.com>
2023-02-16 16:32:47 +0300

    seminar 02 4 is number multiple 7 and 23 done

---
+++ Seminar02-4/Program.cs
@@ -0,0 +1,12 @@
+﻿/*is number multiple 7 and 23*/
+Console.WriteLine("enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number % 7 == 0) && (number % 23 == 0))
+    {
+    Console.WriteLine("number multiple 7 and 23");
+    }
+else
+    {
+    Console.WriteLine("number not multiple 7 and 23");
+    }
+ 
