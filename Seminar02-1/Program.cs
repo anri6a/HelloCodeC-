91ef3504c1ebae5545ef5c0d58b9d13b414752cd
Andrei Shyrakou <anri6a@gmail.com>
2023-02-16 09:39:17 +0300

    seminar 02 1 max of 2 digit done

---
+++ Seminar02-1/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = new Random().Next(10, 99);
+Console.WriteLine("number = "+ number);
+//number = 66;
+int maxDigit = -1;
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if (firstDigit > secondDigit) 
+    maxDigit = firstDigit;
+else if (firstDigit < secondDigit) 
+    maxDigit = secondDigit;
+else 
+    Console.WriteLine("digits are equal");
+
+Console.WriteLine("max digit = " + maxDigit);
+
+
+
+
+
