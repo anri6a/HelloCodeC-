5b0f7935cfccbe2d21daa5fb7b608abb9ea759f2
Andrei Shyrakou <anri6a@gmail.com>
2023-02-16 16:21:41 +0300

    seminar 02 3 is number1 multiple number2 done

---
+++ Seminar02-3/Program.cs
@@ -0,0 +1,12 @@
+﻿/*is number1 multiple number2*/
+Console.WriteLine("enter firstNumber");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter secondNumber");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber % secondNumber == 0)
+Console.WriteLine("Multiple Numbers");
+else 
+    Console.WriteLine("remainder of the division is: " 
+                        + firstNumber % secondNumber);
+
+
