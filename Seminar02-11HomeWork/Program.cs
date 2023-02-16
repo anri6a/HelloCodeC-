405765b6d8858afd51be4288e2315fcb6055cedb
Andrei Shyrakou <anri6a@gmail.com>
2023-02-16 17:38:30 +0300

    seminar 02 11 print 3d digit if exist done

---
+++ Seminar02-11HomeWork/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.*/
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100) 
+Console.WriteLine("ther is no 3d digit");
+else 
+while (number >= 999) 
+number = number / 10;
+Console.WriteLine(number);
+Console.WriteLine("3d digit is " + number % 10);
+
