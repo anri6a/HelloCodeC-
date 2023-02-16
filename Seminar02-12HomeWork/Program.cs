29723c1358f27ab6585fa124b913e50e32980892
Andrei Shyrakou <anri6a@gmail.com>
2023-02-16 22:49:29 +0300

    seminar 02 12 dayNumber is hollyDay done

---
+++ Seminar02-12HomeWork/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот 
+день выходным.*/
+Console.WriteLine("enter dayNumber");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+if (dayNumber <= 7 && dayNumber >= 1)
+{
+if (dayNumber == 6 || dayNumber == 7)
+Console.WriteLine("it is hollyDay");
+else
+Console.WriteLine("it is workDay");
+}
+else 
+Console.WriteLine("it is wrong dayNumber");
+
+
