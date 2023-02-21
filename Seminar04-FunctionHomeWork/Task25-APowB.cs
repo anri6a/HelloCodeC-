bd0da4312fada91dd1fd43052ea7bd8cb5793e98
Andrei Shyrakou <anri6a@gmail.com>
2023-02-21 16:50:19 +0300

    Seminar 04 Finction Home Work A Pow B done

---
+++ Seminar04-FunctionHomeWork/Task25-APowB.cs
@@ -0,0 +1,17 @@
+/*Напишите метод, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+Math.Pow использовать нельзя
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+using static Methods;
+public class Task25APowB
+{
+    public static void APowB()
+    {
+        int A = InputNumber("enter A");
+        int B = InputNumber("enter B");
+        int Result = ResultAPowB(A, B);
+        PrintApowB(Result);
+    }
+}
