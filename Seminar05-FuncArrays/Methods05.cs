86267355630b43db6459cd34712b59cc5f4c66fe
Andrei Shyrakou <anri6a@gmail.com>
2023-02-25 22:57:49 +0300

    add FillArray

--- Seminar05-FuncArrays/Methods05.cs
+++ Seminar05-FuncArrays/Methods05.cs
@@ -4,4 +4,11 @@ public class Methods05
     {
         return new int[size];
     }
+    public static void FillArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next(-9, 9);
+        }
+    }
 }
