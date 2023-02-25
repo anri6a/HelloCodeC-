08831168419af50ae39fe921b18ceace60f9ab1a
Andrei Shyrakou <anri6a@gmail.com>
2023-02-25 23:05:27 +0300

    add Sum Positive elements

--- Seminar05-FuncArrays/Methods05.cs
+++ Seminar05-FuncArrays/Methods05.cs
@@ -11,4 +11,14 @@ public class Methods05
             array[i] = new Random().Next(-9, 9);
         }
     }
+    public static int FindSumPositiveElements(int[] array)
+    {
+        int sumPositive = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i]>0)
+            sumPositive += array[i];
+        }
+        return sumPositive;
+    }
 }
